d2a1ad88fa69e208cee0ad471fc43f701186699e
Euwamahoro <e.uwamahoro@alustudent.com>
2023-11-01 12:45:24 +0200

    This is Enock

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,18 +7,23 @@ import sys
 """Module"""
 
 if __name__ == '__main__':
+    """IF SCRIPT IS NOT RUN AS MODULE"""
     employee_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos/"
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
     user_info = requests.get(user_url).json()
     todos_info = requests.get(todos_url).json()
 
     employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj: obj["completed"], todos_info))
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
 
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
-    for task in task_completed:
-        print(f"\t {task['title']}")
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+
+    [print("\t " + task["title"]) for task in task_completed]
