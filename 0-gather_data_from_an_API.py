8631840b8343f680292eeb2c681aa0aca41f3d5e
Euwamahoro <133766701+Euwamahoro@users.noreply.github.com>
2023-11-02 15:35:52 +0200

    Update 1 0-gather_data_from_an_API.py

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 """
 Script that uses REST API that returns the
-information about the status of employee's TODO list given employee ID.
+information about the status of an employee's TODO list given an employee ID.
 Usage: 0-gather_data_from_an_API.py <employee_id>
 """
 
 import requests
 import sys
 
-
-if __name__ == "__main__":
+if __name__ == "__main":
     if len(sys.argv) != 2:
-        sys.exit("Please provide an employee ID")
+        sys.exit("Usage: 0-gather_data_from_an_API.py <employee_id>")
 
     emp_id = sys.argv[1]
     if not emp_id.isdigit():
         sys.exit("Employee ID must be an integer.")
 
-    response = requests.get("https://jsonplaceholder.typicode.com/todos",
-                            params={"userId": emp_id})
-    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(emp_id))
-    emp_name = employee.json().get('name')
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": emp_id})
+        response.raise_for_status()  # Check for request errors
+        tasks = response.json()
 
-    tasks = response.json()
-    completed = 0
-    for t in tasks:
-        if t.get('completed'):
-            completed += 1
+        employee = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_id}")
+        employee.raise_for_status()  # Check for request errors
+        emp_name = employee.json().get('name')
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(emp_name, completed, len(tasks)))
+        completed = sum(1 for task in tasks if task.get('completed'))
 
-    for t in tasks:
-        if t.get('completed'):
-            print("\t {}".format(t.get('title')))
+        print(f"Employee {emp_name} is done with tasks ({completed}/{len(tasks)}):")
+        for task in tasks:
+            if task.get('completed'):
+                print(f"\t{task.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        sys.exit(f"An error occurred during the API request: {e}")
