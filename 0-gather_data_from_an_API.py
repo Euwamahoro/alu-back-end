d8374abcdcbc3e7633b3e0c3f35d0ca22c2dabf0
Euwamahoro <133766701+Euwamahoro@users.noreply.github.com>
2023-11-02 15:38:30 +0200

    Update 2 0-gather_data_from_an_API.py

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """
-Script that uses REST API that returns the
-information about the status of an employee's TODO list given an employee ID.
-Usage: 0-gather_data_from_an_API.py <employee_id>
+0-gather_data_from_an_API.py
+A Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
 import requests
 import sys
 
-if __name__ == "__main":
-    if len(sys.argv) != 2:
-        sys.exit("Usage: 0-gather_data_from_an_API.py <employee_id>")
 
-    emp_id = sys.argv[1]
-    if not emp_id.isdigit():
-        sys.exit("Employee ID must be an integer.")
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    resp = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".
+                        format(employee_id))
+    todos = resp.json()
 
-    try:
-        response = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": emp_id})
-        response.raise_for_status()  # Check for request errors
-        tasks = response.json()
+    tasks_num = len(todos)
+    tasks_done = 0
 
-        employee = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_id}")
-        employee.raise_for_status()  # Check for request errors
-        emp_name = employee.json().get('name')
+    for todo in todos:
+        if todo["completed"]:
+            tasks_done += 1
+        else:
+            continue
 
-        completed = sum(1 for task in tasks if task.get('completed'))
+    resp = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(employee_id))
+    employee_info = resp.json()
+    name = employee_info["name"]
 
-        print(f"Employee {emp_name} is done with tasks ({completed}/{len(tasks)}):")
-        for task in tasks:
-            if task.get('completed'):
-                print(f"\t{task.get('title')}")
-
-    except requests.exceptions.RequestException as e:
-        sys.exit(f"An error occurred during the API request: {e}")
+    print(f"Employee {name} is done with tasks({tasks_done}/{tasks_num}):")
+    for todo in todos:
+        if todo["completed"]:
+            print("\t {}".format(todo["title"]))
+        else:
+            continue
