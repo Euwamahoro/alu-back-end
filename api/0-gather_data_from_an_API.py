f62107964d5a2a7456509e095c206a682048855c
Euwamahoro <e.uwamahoro@alustudent.com>
2023-11-02 15:53:15 +0200

    This is Enock

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-'''get info from api'''
+"""Module"""
 
-if __name__ == "__main__":
-    import requests
-    from sys import argv
+import requests
+import sys
 
-    url = "https://jsonplaceholder.typicode.com/"
-    userId = argv[1]
+"""Module"""
 
-    user = requests.get(url + "users/{}".format(userId)).json()
-    todos = requests.get(url + "todos?userId={}".format(userId)).json()
+if __name__ == '__main__':
+    """IF SCRIPT IS NOT RUN AS MODULE"""
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
-    name = user.get("name")
-    ct = 0
-    t = len(todos)
-    tasks = ""
+    user_info = requests.get(user_url).json()
+    todos_info = requests.get(todos_url).json()
 
-    for todo in todos:
-        if todo.get("completed"):
-            ct += 1
-            title = todo.get("title")
-            tasks += "\t {}\n".format(title)
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-            line = "Employee {} is done with tasks({}/{})".format(name, ct, t)
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    print(line)
-    print(tasks, end="")
+    [print("\t " + task["title"]) for task in task_completed]
