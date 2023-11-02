9cee49b0041874bf302f998d8134bd22f6c34661
Euwamahoro <e.uwamahoro@alustudent.com>
2023-11-02 16:27:10 +0200

    This is Enock 2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""Fetch user TODO list progress
-
-From https://jsonplaceholder.typicode.com/users/1/todos
 """
-import requests
-import sys
+place holder
+"""
 
-# Disable module execution, only run directly
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        # Get command arguments
-        user_id = sys.argv[1]
 
-        # Enpoints urls
-        api_url = 'https://jsonplaceholder.typicode.com/'
-        user_url = api_url + 'users/{}'.format(user_id)
-        user_todos_url = api_url + 'users/{}/todos'.format(user_id)
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
 
-        # Request data
-        user = requests.get(user_url).json()
-        todos = requests.get(user_todos_url).json()
-
-        # Process data
-        titles = [todo.get('title') for todo in todos if todo.get('completed')]
-
-        # Output data
-        print("Employee {} is done with tasks({}/20):".format(
-            user.get('name'), len(titles), len(todos)))
-        print('\n'.join(['\t {}'.format(title) for title in titles]))
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
