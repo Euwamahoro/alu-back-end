2f5563c40dd33f919438909164af37f1d17db696
Euwamahoro <e.uwamahoro@alustudent.com>
2023-11-02 19:03:45 +0200

    This is Enock 3

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -23,5 +23,4 @@ if __name__ == "__main__":
     file_name = "{}.csv".format(argv[1])
     with open(file_name, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow([argv[1], name, todo['completed'], todo['title']])
+        for todo in todos: writer.writerow([argv[1], name, todo['completed'], todo['title']])
